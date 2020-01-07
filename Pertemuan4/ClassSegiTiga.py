cf97fbfd16fb732743fe3997138e5f2fe6c17545
zulfa27 <56072254+zulfa27@users.noreply.github.com>
2020-01-07 18:43:11 +0700

    Add files via upload

---
+++ Pertemuan4/ClassSegiTiga.py
@@ -0,0 +1,7 @@
+class SegiTiga:
+    def __init__(self, alas, tinggi):
+        self.__alas = alas
+        self.__tinggi = tinggi
+
+    def Luas(self):
+        return (self.__alas * self.__tinggi) / 2
